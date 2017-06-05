37e4543052830867fb8a3b2e58cefb4f9b3af206
Fernandez Ludovic <lfernandez.dev@gmail.com>
2017-06-05 17:12:05 +0200

    refactor: extract types.

--- config/config.go
+++ config/config.go
@@ -1,11 +1,11 @@
+// Config https://git-scm.com/docs/git-config
 package config
 
-import "github.com/ldez/go-git-cmd-wrapper/git"
+import "github.com/ldez/go-git-cmd-wrapper/types"
 
-// Config https://git-scm.com/docs/git-config
 
-func Entry(key string, value string) func(*git.Cmd) {
-	return func(g *git.Cmd) {
+func Entry(key string, value string) func(*types.Cmd) {
+	return func(g *types.Cmd) {
 		g.AddOptions(key)
 		g.AddOptions(value)
 	}
