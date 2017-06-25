9fc5c759792bc33ef79faf742900fc99e33b1fb8
Fernandez Ludovic <lfernandez.dev@gmail.com>
2017-06-25 20:13:21 +0200

    doc(remote): Add Go style package doc.

---
+++ remote/doc.go
@@ -0,0 +1,26 @@
+/*
+git-remote - Manage set of tracked repositories
+
+SYNOPSIS
+
+		git remote [-v | --verbose]
+		git remote add [-t <branch>] [-m <master>] [-f] [--[no-]tags] [--mirror=<fetch|push>] <name> <url>
+		git remote rename <old> <new>
+		git remote remove <name>
+		git remote set-head <name> (-a | --auto | -d | --delete | <branch>)
+		git remote set-branches [--add] <name> <branch>...
+		git remote get-url [--push] [--all] <name>
+		git remote set-url [--push] <name> <newurl> [<oldurl>]
+		git remote set-url --add [--push] <name> <newurl>
+		git remote set-url --delete [--push] <name> <url>
+		git remote [-v | --verbose] show [-n] <name>...
+		git remote prune [-n | --dry-run] <name>...
+		git remote [-v | --verbose] update [-p | --prune] [(<group> | <remote>)...]
+
+DESCRIPTION
+
+Manage the set of repositories ("remotes") whose branches you track.
+
+URL https://git-scm.com/docs/git-remote
+*/
+package remote
--- remote/remote.go
+++ remote/remote.go
@@ -1,17 +1,3 @@
-// Remote https://git-scm.com/docs/git-remote
-//	git remote [-v | --verbose]
-//	git remote add [-t <branch>] [-m <master>] [-f] [--[no-]tags] [--mirror=<fetch|push>] <name> <url>
-//	git remote rename <old> <new>
-//	git remote remove <name>
-//	git remote set-head <name> (-a | --auto | -d | --delete | <branch>)
-//	git remote set-branches [--add] <name> <branch>…​
-//	git remote get-url [--push] [--all] <name>
-//	git remote set-url [--push] <name> <newurl> [<oldurl>]
-//	git remote set-url --add [--push] <name> <newurl>
-//	git remote set-url --delete [--push] <name> <url>
-//	git remote [-v | --verbose] show [-n] <name>…​
-//	git remote prune [-n | --dry-run] <name>…​
-//	git remote [-v | --verbose] update [-p | --prune] [(<group> | <remote>)…​]
 package remote
 
 import (
