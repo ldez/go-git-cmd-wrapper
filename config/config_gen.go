3c9f1be229486c985f17dd6089065c9d8d70c415
Fernandez Ludovic <lfernandez.dev@gmail.com>
2017-07-02 21:38:39 +0200

    refactor(config): separate generated content and custom content to files.

---
+++ config/config_gen.go
@@ -0,0 +1,97 @@
+/*
+* CODE GENERATED AUTOMATICALLY
+* THIS FILE MUST NOT BE EDITED BY HAND
+ */
+package config
+
+import "github.com/ldez/go-git-cmd-wrapper/types"
+
+// Blob Similar to --file but use the given blob instead of a file. E.g. you can use master:.gitmodules to read values from the file .gitmodules in the master branch. See 'SPECIFYING REVISIONS' section in gitrevisions(7) for a more complete list of ways to spell blob names. 
+// --blob <blob>
+func Blob(value string) func(*types.Cmd) {
+	return func(g *types.Cmd) {
+		g.AddOptions("--blob")
+		g.AddOptions(value)
+	}
+}
+
+// Bool git config will ensure that the output is 'true' or 'false' 
+// --bool
+func Bool(g *types.Cmd) {
+	g.AddOptions("--bool")
+}
+
+// BoolOrInt git config will ensure that the output matches the format of either --bool or --int, as described above. 
+// --bool-or-int
+func BoolOrInt(g *types.Cmd) {
+	g.AddOptions("--bool-or-int")
+}
+
+// Edit Opens an editor to modify the specified config file; either --system, --global, or repository (default). 
+// -e, --edit
+func Edit(g *types.Cmd) {
+	g.AddOptions("--edit")
+}
+
+// File Use the given config file instead of the one specified by GIT_CONFIG. 
+// -f <config-file>, --file <config-file>
+func File(configFile string) func(*types.Cmd) {
+	return func(g *types.Cmd) {
+		g.AddOptions("--file")
+		g.AddOptions(configFile)
+	}
+}
+
+// Global For writing options: write to global ~/.gitconfig file rather than the repository .git/config, write to $XDG_CONFIG_HOME/git/config file if this file exists and the ~/.gitconfig file doesn’t. 
+// --global
+func Global(g *types.Cmd) {
+	g.AddOptions("--global")
+}
+
+// Int git config will ensure that the output is a simple decimal number. An optional value suffix of k, m, or g in the config file will cause the value to be multiplied by 1024, 1048576, or 1073741824 prior to output. 
+// --int
+func Int(g *types.Cmd) {
+	g.AddOptions("--int")
+}
+
+// List List all variables set in config file, along with their values. 
+// -l, --list
+func List(g *types.Cmd) {
+	g.AddOptions("--list")
+}
+
+// Local For writing options: write to the repository .git/config file. This is the default behavior. 
+// --local
+func Local(g *types.Cmd) {
+	g.AddOptions("--local")
+}
+
+// NameOnly Output only the names of config variables for --list or --get-regexp. 
+// --name-only
+func NameOnly(g *types.Cmd) {
+	g.AddOptions("--name-only")
+}
+
+// Null For all options that output values and/or keys, always end values with the null character (instead of a newline). Use newline instead as a delimiter between key and value. This allows for secure parsing of the output without getting confused e.g. by values that contain line breaks. 
+// -z, --null
+func Null(g *types.Cmd) {
+	g.AddOptions("--null")
+}
+
+// Path git-config will expand leading ~ to the value of $HOME, and ~user to the home directory for the specified user. This option has no effect when setting the value (but you can use git config bla ~/ from the command line to let your shell do the expansion). 
+// --path
+func Path(g *types.Cmd) {
+	g.AddOptions("--path")
+}
+
+// ShowOrigin Augment the output of all queried config options with the origin type (file, standard input, blob, command line) and the actual origin (config file path, ref, or blob id if applicable). 
+// --show-origin
+func ShowOrigin(g *types.Cmd) {
+	g.AddOptions("--show-origin")
+}
+
+// System For writing options: write to system-wide $(prefix)/etc/gitconfig rather than the repository .git/config. 
+// --system
+func System(g *types.Cmd) {
+	g.AddOptions("--system")
+}
---
+++ config/doc.go
@@ -0,0 +1,53 @@
+/*
+git-config - Get and set repository or global options
+
+SYNOPSIS
+
+		git config [<file-option>] [type] [--show-origin] [-z|--null] name [value [value_regex]]
+		git config [<file-option>] [type] --add name value
+		git config [<file-option>] [type] --replace-all name value [value_regex]
+		git config [<file-option>] [type] [--show-origin] [-z|--null] --get name [value_regex]
+		git config [<file-option>] [type] [--show-origin] [-z|--null] --get-all name [value_regex]
+		git config [<file-option>] [type] [--show-origin] [-z|--null] [--name-only] --get-regexp name_regex [value_regex]
+		git config [<file-option>] [type] [-z|--null] --get-urlmatch name URL
+		git config [<file-option>] --unset name [value_regex]
+		git config [<file-option>] --unset-all name [value_regex]
+		git config [<file-option>] --rename-section old_name new_name
+		git config [<file-option>] --remove-section name
+		git config [<file-option>] [--show-origin] [-z|--null] [--name-only] -l | --list
+		git config [<file-option>] --get-color name [default]
+		git config [<file-option>] --get-colorbool name [stdout-is-tty]
+		git config [<file-option>] -e | --edit
+
+DESCRIPTION
+
+You can query/set/replace/unset options with this command. The name is actually the section and the key separated by a dot, and the value will be escaped.
+
+Multiple lines can be added to an option by using the --add option.
+If you want to update or unset an option which can occur on multiple lines, a POSIX regexp value_regex needs to be given.
+Only the existing values that match the regexp are updated or unset.
+If you want to handle the lines that do not match the regex, just prepend a single exclamation mark in front (see also the section called “EXAMPLES”).
+
+The type specifier can be either --int or --bool, to make git config ensure that the variable(s) are of the given type and convert the value to the canonical form (simple decimal number for int, a "true" or "false" string for bool), or --path, which does some path expansion (see --path below).
+If no type specifier is passed, no checks or transformations are performed on the value.
+
+When reading, the values are read from the system, global and repository local configuration files by default, and options --system, --global, --local and --file <filename> can be used to tell the command to read from only that location (see the section called “FILES”).
+
+When writing, the new value is written to the repository local configuration file by default, and options --system, --global, --file <filename> can be used to tell the command to write to that location (you can say --local but that is the default).
+
+This command will fail with non-zero status upon error. Some exit codes are:
+
+- The section or key is invalid (ret=1),
+- no section or name was provided (ret=2),
+- the config file is invalid (ret=3),
+- the config file cannot be written (ret=4),
+- you try to unset an option which does not exist (ret=5),
+- you try to unset/set an option for which multiple lines match (ret=5), or
+- you try to use an invalid regexp (ret=6).
+
+On success, the command returns the exit code 0.
+
+
+URL https://git-scm.com/docs/git-config
+*/
+package config
