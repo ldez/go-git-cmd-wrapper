590e8f3c63daa8e76077fa9c8c2446c421a15ef1
Fernandez Ludovic <lfernandez.dev@gmail.com>
2017-07-28 15:52:50 +0200

    doc(pull): Add doc URL.

--- pull/doc.go
+++ pull/doc.go
@@ -38,5 +38,7 @@ hard to back out of in the case of a conflict.
 
 If any of the remote changes overlap with local uncommitted changes, the merge will be automatically canceled and the work tree untouched. It is generally best to get any local changes in working order before pulling or stash
 them away with git-stash(1).
+
+URL:	https://git-scm.com/docs/git-pull
 */
 package pull
